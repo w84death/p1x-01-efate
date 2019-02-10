af78a1f2693007644c628715dd17c75ea361f5ee
Krzysztof Jankowski <krzysztof.jankowski@pearson.com>
2019-02-10 20:49:25 +0100

    menu style, RMB to back

--- scripts/back_to_main.gd
+++ scripts/back_to_main.gd
@@ -4,8 +4,9 @@ func _ready():
     pass # Replace with function body.
 
 func _input(event):
-    if Input.is_key_pressed(KEY_ESCAPE):
+    if Input.is_key_pressed(KEY_ESCAPE) or Input.is_mouse_button_pressed(BUTTON_RIGHT):
         back()
+        
         
 func back():
     get_tree().change_scene("scenes/main.tscn")
